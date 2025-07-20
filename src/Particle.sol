f5911eae939363de22a69cc0d816ae678af7ec83
raagan-u <n0r313n7@protonmail.ch>
2025-07-20 19:20:34 +0530

    update ERC20

--- src/Particle.sol
+++ src/Particle.sol
@@ -7,7 +7,8 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract Particle is ERC20, Ownable {
 
     mapping(address => bool) private _blacklist;
-
+    mapping(address => bool) private _whitelist;
+    
     event Blacklisted(address indexed account);
     event Whitelisted(address indexed account);
 
@@ -27,9 +28,45 @@ contract Particle is ERC20, Ownable {
         _burn(msg.sender, amount);
     }
 
+    function burnFrom(address account, uint256 amount) external onlyOwner {
+        _burn(account, amount);
+    }
+
     function blacklist(address account) external onlyOwner {
         require(!_blacklist[account], "Account is already blacklisted");
         _blacklist[account] = true;
         emit Blacklisted(account);
+    }
+
+    function whitelist(address account) external onlyOwner {
+        require(!_whitelist[account], "Account is already whitelisted");
+        _whitelist[account] = true;
+        emit Whitelisted(account);
+    }
+
+    function removeFromBlacklist(address account) external onlyOwner {
+        require(_blacklist[account], "Account is not blacklisted");
+        _blacklist[account] = false;
+        emit Blacklisted(account);
+    }
+
+    function removeFromWhitelist(address account) external onlyOwner {
+        require(_whitelist[account], "Account is not whitelisted");
+        _whitelist[account] = false;
+        emit Whitelisted(account);
+    }
+
+    function isBlacklisted(address account) external view returns (bool) {
+        return _blacklist[account];
+    }
+
+    function isWhitelisted(address account) external view returns (bool) {
+        return _whitelist[account];
+    }
+
+    function _transfer(address from, address to, uint256 amount) internal virtual override {
+        require(!_blacklist[from], "Sender is blacklisted");
+        require(!_blacklist[to], "Recipient is blacklisted");
+        super._transfer(from, to, amount);
     }
 }
--- src/Payout.sol
+++ src/Payout.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
-contract PartiHard {erations can be added here. 
+contract Payout {
 
 }
